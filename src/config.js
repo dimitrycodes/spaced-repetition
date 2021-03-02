88174fe2a18e944799b8fb07a36cd39677f3f0b8
Dimitry A <strongsafetyking@yahoo.com>
2021-03-02 15:32:08 -0500

    enpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api', //https://aqueous-reef-27793.herokuapp.com/api
+  API_ENDPOINT: 'https://aqueous-reef-27793.herokuapp.com/api', //http://localhost:8000/api
   TOKEN_KEY: 'spacedreppetition-client-auth-token',
 }
