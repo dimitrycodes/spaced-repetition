7bb898cc84f76fc133bb1cb47f0f748bb35a1437
dimitrycodes <strongafetyking@yahoo.com>
2021-03-02 17:24:33 +0530

    register

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -6,107 +6,117 @@ import UserContext from '../../contexts/UserContext'
 import Button from '../Button/Button'
 import './RegistrationForm.css'
 
+
 class RegistrationForm extends Component {
-  static defaultProps = {
-    onRegistrationSuccess: () => { }
-  }
-  static contextType = UserContext
+    static defaultProps = {
+        onRegistrationSuccess: () => {}
+    }
+    static contextType = UserContext
 
-  state = { error: null }
+    state = { error: null }
 
-  firstInput = React.createRef()
+    firstInput = React.createRef()
 
-  handleSubmit = ev => {
-    ev.preventDefault()
-    const { name, username, password } = ev.target
-    AuthApiService.postUser({
-      name: name.value,
-      username: username.value,
-      password: password.value,
-    })
-      .then(user => {
-        this.handleLogin(username.value, password.value);
-        name.value = ''
-        username.value = ''
-        password.value = '';
-        this.props.onRegistrationSuccess()
-      })
-      .catch(res => {
-        this.setState({ error: res.error })
-      })
-  }
+    handleSubmit = ev => {
+        ev.preventDefault()
+        const { name, username, password } = ev.target
+        AuthApiService.postUser({
+                name: name.value,
+                username: username.value,
+                password: password.value,
+            })
+            .then(user => {
+                this.handleLogin(username.value, password.value);
+                name.value = ''
+                username.value = ''
+                password.value = '';
+                this.props.onRegistrationSuccess()
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
+    }
 
-  handleLogin = (username, password) => {
-    this.setState({ error: null })
+    handleLogin = (username, password) => {
+        this.setState({ error: null })
 
-    AuthApiService.postLogin({
-      username: username,
-      password: password,
-    })
-      .then(res => {
-        this.context.processLogin(res.authToken)
-      })
-      .then(() => this.props.onLoginSuccess())
-      .catch(res => {
-        this.setState({ error: res.error })
-      })
-  }
+        AuthApiService.postLogin({
+                username: username,
+                password: password,
+            })
+            .then(res => {
+                this.context.processLogin(res.authToken)
+            })
+            .then(() => this.props.onLoginSuccess())
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
+    }
 
-  componentDidMount() {
-    this.firstInput.current.focus()
-  }
+    componentDidMount() {
+        this.firstInput.current.focus()
+    }
 
-  render() {
-    const { error } = this.state
-    return (
-      <form
-        onSubmit={this.handleSubmit}
-      >
-        <div role='alert'>
-          {error && <p>{error}</p>}
-        </div>
-        <div>
-          <Label htmlFor='registration-name-input'>
-            Enter your name<Required />
-          </Label>
-          <Input
-            ref={this.firstInput}
-            id='registration-name-input'
-            name='name'
-            required
-          />
-        </div>
-        <div>
-          <Label htmlFor='registration-username-input'>
-            Choose a username<Required />
-          </Label>
-          <Input
-            id='registration-username-input'
-            name='username'
-            required
-          />
-        </div>
-        <div>
-          <Label htmlFor='registration-password-input'>
-            Choose your password<Required />
-          </Label>
-          <Input
-            id='registration-password-input'
-            name='password'
-            type='password'
-            required
-          />
-        </div>
-        <div>
-          <Button type='submit'>
-            Sign up
-          </Button>
-          {' '}
-          <Link to='/login'>Click her if have an account or just want to demo!</Link>
-        </div>
-      </form>
-    )
-  }
-}
+    render() {
+        const { error } = this.state
+        return ( <
+            form onSubmit = { this.handleSubmit } >
+            <
+            div role = 'alert' > {
+                error && < p > { error } < /p>} <
+                /div> <
+                div >
+                <
+                Label htmlFor = 'registration-name-input' >
+                Enter your name < Required / >
+                <
+                /Label> <
+                Input
+                ref = { this.firstInput }
+                id = 'registration-name-input'
+                name = 'name'
+                required /
+                >
+                <
+                /div> <
+                div >
+                <
+                Label htmlFor = 'registration-username-input' >
+                Choose a username < Required / >
+                <
+                /Label> <
+                Input
+                id = 'registration-username-input'
+                name = 'username'
+                required /
+                >
+                <
+                /div> <
+                div >
+                <
+                Label htmlFor = 'registration-password-input' >
+                Choose your password < Required / >
+                <
+                /Label> <
+                Input
+                id = 'registration-password-input'
+                name = 'password'
+                type = 'password'
+                required /
+                >
+                <
+                /div> <
+                div >
+                <
+                Button type = 'submit' >
+                Sign up <
+                /Button> { ' ' } <
+                Link to = '/login' > Click her
+                if have an account or just want to demo! < /Link> <
+                /div> <
+                /form>
+            )
+        }
+    }
 
-export default RegistrationForm
+    export default RegistrationForm
